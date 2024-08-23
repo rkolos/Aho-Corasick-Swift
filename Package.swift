80f8a348edf3f6f6f494a82c7aba6c8bcce70373
Roman <44463575+rkolos@users.noreply.github.com>
2024-08-23 13:34:07 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,27 @@
 import PackageDescription
 
 let package = Package(
-  name: "Aho-Corasick"
+    name: "Aho-Corasick",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AhoCorasick",
+            targets: ["AhoCorasick"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "AhoCorasick",
+            path: "Source"  // Убедитесь, что путь "Source" существует и содержит исходные файлы
+        ),
+        .testTarget(
+            name: "AhoCorasickTests",
+            dependencies: ["AhoCorasick"],
+            path: "Tests"  // Убедитесь, что путь "Tests" существует и содержит тестовые файлы
+        )
+    ]
 )
+
