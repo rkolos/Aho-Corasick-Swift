18060a3526269bc17a8d5c70d07ad8d0dce0c493
Roman <44463575+rkolos@users.noreply.github.com>
2024-08-23 13:29:12 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Aho-Corasick",
-    platforms: [
-        .macOS(.v10_15),   // Укажите минимальные версии платформ, если необходимо
-        .iOS(.v13)
-    ],
-    products: [
-        .library(
-            name: "AhoCorasick",
-            targets: ["AhoCorasick"])
-    ]
+  name: "Aho-Corasick"
 )
